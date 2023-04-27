70a48a2264f206c5392e9f0f7864ee1245c3c543
HusseinAlaa <hussein4alaa@gmail.com>
2023-04-27 17:38:30 +0300

    Update KeycloakGuard.php

--- src/KeycloakGuard.php
+++ src/KeycloakGuard.php
@@ -5,8 +5,11 @@ namespace g4t\Keycloak;
 use Illuminate\Auth\GuardHelpers;
 use Illuminate\Contracts\Auth\Guard;
 use App\Auth\CustomUserProvider;
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 
 class KeycloakGuard implements Guard
 {
@@ -16,6 +19,7 @@ class KeycloakGuard implements Guard
     private $decodedToken;
     private $config;
 
+
     public function __construct(UserProvider $provider)
     {
         $this->config = config('g4t-keycloak');
@@ -24,6 +28,7 @@ class KeycloakGuard implements Guard
         $this->user = null;
         $this->authenticate();
     }
+
 
     private function authenticate()
     {
@@ -36,36 +41,52 @@ class KeycloakGuard implements Guard
         }
     }
 
+
     public function check()
     {
         return !is_null($this->user());
     }
+
 
     public function guest()
     {
         return is_null($this->user());
     }
 
+
+    public function checkTokenExpiration()
+    {
+        if($this->decodedToken) {
+            $timestampInMs = $this->decodedToken->exp;
+            $date = Carbon::createFromTimestampMs($timestampInMs);
+            $now = Carbon::now();
+            $expiration_date = $date->format('Y-m-d h:i:s');
+            if($expiration_date < $now) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+
     public function user()
     {
-        if (is_null($this->user)) {
-            return null;
+        if($this->config['make_keycloak_token_expired']) {
+            $expired = $this->checkTokenExpiration();
+            if($expired) {
+                return null;
+            }
         }
+
         if($this->config['load_user_from_database']) {
             return $this->user;
+        } else {
+            return $this->getUserFromKeycloak();
         }
-
-        if (!is_null($this->decodedToken)) {
-            return (object) [
-                'id' => $this->decodedToken->sub,
-                'name' => $this->decodedToken->name,
-                'username' => $this->decodedToken->preferred_username,
-                'given_name' => $this->decodedToken->given_name,
-            ];
-        }
-
         return null;
     }
+
 
     public function id()
     {
@@ -76,6 +97,7 @@ class KeycloakGuard implements Guard
         return null;
     }
 
+
     public function validate($credentials = [])
     {
         $class = $this->provider->getModel();
@@ -85,12 +107,13 @@ class KeycloakGuard implements Guard
         return true;
     }
 
+
     public function setUser(Authenticatable $user)
     {
         $this->user = $user;
     }
-    
-    
+
+
     public function attempt(array $credentials)
     {
         if(!array_key_exists('url', $credentials)) {
@@ -106,6 +129,19 @@ class KeycloakGuard implements Guard
         $response = Http::asForm()->post($url, $credentials)->json();
 
         return $response;
+    }
+
+
+    public function getUserFromKeycloak() {
+        if (!is_null($this->decodedToken)) {
+            return (object) [
+                'id' => $this->decodedToken->sub,
+                'name' => $this->decodedToken->name,
+                'username' => $this->decodedToken->preferred_username,
+                'given_name' => $this->decodedToken->given_name,
+            ];
+        }
+        return null;
     }
 
 }
